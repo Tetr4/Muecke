eff8bb84aebfaa066d11cf7b7a38ca2556dfb682
Mike Klimek <mike.klimek.developer@gmail.com>
2021-02-26 19:02:44 +0100

    feat: add more puppy data

--- app/src/main/java/com/example/androiddevchallenge/data/Puppies.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/Puppies.kt
@@ -18,12 +18,54 @@ package com.example.androiddevchallenge.data
 
 import com.example.androiddevchallenge.R
 
-val PUPPIES = (1..30).map {
-    Puppy(
-        id = it,
-        name = "Mücke $it",
-        caption = "Actually not a puppy",
-        description = "Just look at those ears! Also not up for adoption anymore.",
-        image = R.drawable.muecke,
-    )
-}
+private var id = 0
+
+private val prototype = Puppy(
+    id = id++,
+    name = "Mücke",
+    description = "Just look at those ears! Also not up for adoption anymore.",
+    image = R.drawable.muecke_portrait,
+)
+
+val PUPPIES = listOf(
+    prototype.copy(
+        id = id++,
+        caption = "On a pillow",
+        image = R.drawable.muecke_pillow
+    ),
+    prototype.copy(
+        id = id++,
+        caption = "In a basket",
+        image = R.drawable.muecke_basket
+    ),
+    prototype.copy(
+        id = id++,
+        caption = "In a box",
+        image = R.drawable.muecke_box
+    ),
+    prototype.copy(
+        id = id++,
+        caption = "In the grass",
+        image = R.drawable.muecke_field
+    ),
+    prototype.copy(
+        id = id++,
+        caption = "Just happy",
+        image = R.drawable.muecke_happy
+    ),
+    prototype.copy(
+        id = id++,
+        caption = "On a llama",
+        image = R.drawable.muecke_llama
+    ),
+    prototype.copy(
+        id = id++,
+        name = "Mücke and Schnitzel",
+        image = R.drawable.muecke_schnitzel
+    ),
+    prototype.copy(
+        id = id++,
+        caption = "Sleeping",
+        image = R.drawable.muecke_sleepy
+    ),
+)
